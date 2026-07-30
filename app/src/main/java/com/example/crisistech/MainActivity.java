82e546f2e8f0f12fc62196c2d2429f86472bfe59
gitmahin <nimahin25@gmail.com>
2026-07-30 18:35:59 +0600

    chore: add profile section

--- app/src/main/java/com/example/crisistech/MainActivity.java
+++ app/src/main/java/com/example/crisistech/MainActivity.java
@@ -1,61 +1,94 @@
 package com.example.crisistech;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout mainDrawableMenu;
     Button mainMenuOpenButton;
-    Button wifiEnableButton;
+
+    NavigationView mainNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-//        mainDrawableMenu = findViewById(R.id.mainMenuDrawerLayout);
-        mainMenuOpenButton =  findViewById(R.id.wifiEnableButton);
-        wifiEnableButton = findViewById(R.id.wifiEnableButton);
+        mainDrawableMenu = findViewById(R.id.mainMenuDrawerLayout);
+        mainMenuOpenButton = findViewById(R.id.menu_open_button);
+        mainNavigationView = findViewById(R.id.main_navigation_view);
 
-        this.init();
+        this.mainMenuManager();
+        this.mainNavigationViewManager();
     }
 
-    public void init() {
-//        mainMenuOpenButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                mainDrawableMenu.open();
-//            }
-//        });
-
-        wifiEnableButton.setOnClickListener(new View.OnClickListener() {
+    private void mainMenuManager() {
+        mainMenuOpenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Drawable enabledBg = ContextCompat.getDrawable(MainActivity.this, R.drawable.wifi_enable_button);
-                Drawable disabledBg = ContextCompat.getDrawable(MainActivity.this, R.drawable.wifi_button);
-                if(wifiEnableButton.getText().toString().toLowerCase().equals("wifi on")) {
-                wifiEnableButton.setText("Wifi Off");
-                wifiEnableButton.setBackground(disabledBg);
-                } else {
-                    wifiEnableButton.setText("Wifi On");
-                    wifiEnableButton.setBackground(enabledBg);
+                mainDrawableMenu.open();
+            }
+        });
+
+
+    }
+
+
+    private void mainNavigationViewManager() {
+
+        mainNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                Fragment fragment = new HomeFragment();
+                ;
+
+                if (itemId == R.id.nav_home) {
+                    fragment = new HomeFragment();
                 }
+
+                if (itemId == R.id.nav_profile) {
+                    fragment = new ProfileFragment();
+                }
+
+                if (itemId == R.id.nav_active_devices) {
+                    Toast.makeText(MainActivity.this, "Active Devices", Toast.LENGTH_SHORT).show();
+                }
+
+                if (itemId == R.id.nav_terms_conditions) {
+                    Toast.makeText(MainActivity.this, "Terms & Conditions", Toast.LENGTH_SHORT).show();
+                }
+
+                if (itemId == R.id.nav_privacy_policy) {
+                    Toast.makeText(MainActivity.this, "Privacy Policy", Toast.LENGTH_SHORT).show();
+                }
+
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainer, fragment)
+                        .commit();
+
+
+                mainDrawableMenu.close();
+                return true;
             }
         });
     }
